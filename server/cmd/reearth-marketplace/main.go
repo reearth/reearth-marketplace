6c49d072ee9e76b529728788bc4c119d159524b7
HideBa <baba.papa1120.ba@gmail.com>
2022-08-07 18:43:48 +0900

    add build and ci

--- server/cmd/reearth-marketplace/main.go
+++ server/cmd/reearth-marketplace/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
 	println("hello, world.")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, "hello")
+	})
+	e.Start(":8080")
 }
