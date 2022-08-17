2157daa80c24766cdcbbcc65baf4cb20e24a4d00
HideBa <49897538+HideBa@users.noreply.github.com>
2022-08-17 14:47:25 +0900

    ci: setup workflow to run CI by detecting file changes, build and deploy (#4)
    Co-authored-by: rot1024 <aayhrot@gmail.com>

--- server/cmd/reearth-marketplace/main.go
+++ server/cmd/reearth-marketplace/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
 	println("hello, world.")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, "hello")
+	})
+	err := e.Start(":8080")
+	if err != nil {
+		panic("failed to run server")
+	}
 }
