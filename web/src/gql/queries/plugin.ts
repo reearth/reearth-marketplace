d0f5d19fc49d9caefae158665d6669713e4fe2eb
Shinnosuke Komiya <kurohune538@gmail.com>
2022-08-30 17:57:42 -0400

    fix(web): fix linter

--- web/src/gql/queries/plugin.ts
+++ web/src/gql/queries/plugin.ts
@@ -1,44 +1,62 @@
-
 import { gql } from "@apollo/client";
 
 const SEARCH_PLUGIN = gql`
-    query SearchPlugin($first: Int!, $keyword: String, $liked: Boolean, $tags: [String!], $types: [PluginType!], $publisher: ID, $sort: PluginSort, $after: Cursor) {
-        plugins( input: {keyword: $keyword, liked: $liked, tags: $tags, types: $types, publisher: $publisher, sort: $sort, after: $after } ) {
-            nodes {
-                id
-                images
-                author
-                like
-                downloads
-                name
-            }
-            pageInfo {
-                endCursor
-                hasNextPage
-            }
-            totalCount
-        } 
+  query SearchPlugin(
+    $first: Int!
+    $keyword: String
+    $liked: Boolean
+    $tags: [String!]
+    $types: [PluginType!]
+    $publisher: ID
+    $sort: PluginSort
+    $after: Cursor
+  ) {
+    plugins(
+      input: {
+        keyword: $keyword
+        liked: $liked
+        tags: $tags
+        types: $types
+        publisher: $publisher
+        sort: $sort
+        after: $after
+      }
+    ) {
+      nodes {
+        id
+        images
+        author
+        like
+        downloads
+        name
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      totalCount
     }
-`
+  }
+`;
 
 const LIKE_PLUGIN = gql`
-    mutation LikePlugin($id: ID!) {
-        likePlugin( input: { pluginId: $id } ) {
-            plugin {
-                id
-                like
-            }
-        }
+  mutation LikePlugin($id: ID!) {
+    likePlugin(input: { pluginId: $id }) {
+      plugin {
+        id
+        like
+      }
     }
-`
+  }
+`;
 
 const UNLIKE_PLUGIN = gql`
-    mutation UnlikePlugin($id: ID!) {
-        unlikePlugin( input: { pluginId: $id } ) {
-            plugin {
-                id
-                like
-            }
-        }
+  mutation UnlikePlugin($id: ID!) {
+    unlikePlugin(input: { pluginId: $id }) {
+      plugin {
+        id
+        like
+      }
     }
-`
+  }
+`;
