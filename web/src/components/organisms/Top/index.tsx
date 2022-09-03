2f2aa07be97cb4844ab5b3ab7eaeb8b27a8e47fb
Shinnosuke Komiya <>
2022-09-04 01:52:45 +0200

    fix(web): fix test env

--- web/src/components/organisms/Top/index.tsx
+++ web/src/components/organisms/Top/index.tsx
@@ -1,19 +1,20 @@
-import { useAuth } from "@marketplace/auth/hooks";
+// import { useAuth } from "@marketplace/auth/hooks";
 import TopPage from "@marketplace/components/molecules/TopPage";
-import { useState } from "react";
+// import { useState } from "react";
 
 import useHooks from "./hooks";
 
 export type Props = {};
 
 const Top: React.FC<Props> = () => {
-  const { isAuthenticated } = useAuth();
-  const [searchText, updateSearchText] = useState<string>("");
-  const handleSearch = (text: string) => {
-    updateSearchText(text);
-  };
-  const { plugins } = useHooks(searchText);
-  return <TopPage isLoggedIn={isAuthenticated} plugins={plugins} onSearch={handleSearch} />;
+  // const { isAuthenticated } = useAuth();
+  // const [searchText, updateSearchText] = useState<string>("");
+  // const handleSearch = (text: string) => {
+  //   updateSearchText(text);
+  // };
+  // const { plugins } = useHooks(searchText);
+  const { plugins } = useHooks();
+  return <TopPage plugins={plugins} />;
 };
 
 export default Top;
