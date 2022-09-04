e6c86e16d90d22032dca20ebec92a1d12e8a88f1
KaWaite <kyle.waite@outlook.com>
2022-09-04 15:50:24 +0900

    simplify marketplacecore

--- web/src/components/MarketplaceCore.tsx
+++ web/src/components/MarketplaceCore.tsx
@@ -1,4 +1,6 @@
 import CoreWrapper from "@marketplace/components/molecules/Common/CoreWrapper";
+import Footer from "@marketplace/components/molecules/Common/Footer";
+import Header from "@marketplace/components/organisms/Common/Header";
 import AddNewPlugin from "@marketplace/components/pages/AddNewPlugin";
 import PublisherRegistration from "@marketplace/components/pages/DeveloperRegistration";
 import MyPlugins from "@marketplace/components/pages/MyPlugins";
@@ -7,18 +9,11 @@ import PluginDetailPage from "@marketplace/components/pages/PluginDetail";
 import RootPage from "@marketplace/components/pages/Root";
 import UpdatePlugin from "@marketplace/components/pages/UpdatePlugin";
 import UserPage from "@marketplace/components/pages/User";
-import { ComponentType } from "react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
-export type Props = {
-  header?: ComponentType;
-  footer?: ComponentType;
-  customFallback?: ComponentType;
-};
-
-const AppRoutes = ({ customFallback: CustomFallback }: { customFallback?: ComponentType }) => {
+const AppRoutes = () => {
   return useRoutes([
-    { path: "/", element: <RootPage /> },
+    { path: "/", element: <RootPage showBanner /> },
     { path: "/plugins/:pluginId", element: <PluginDetailPage /> },
     { path: "/:userId", element: <UserPage /> },
     {
@@ -28,15 +23,15 @@ const AppRoutes = ({ customFallback: CustomFallback }: { customFallback?: Compon
     { path: "/myplugins", element: <MyPlugins /> },
     { path: "/myplugins/new", element: <AddNewPlugin /> },
     { path: "/myplugins/:pluginId/update", element: <UpdatePlugin /> },
-    { path: "*", element: CustomFallback ? <CustomFallback /> : <NotFound /> },
+    { path: "*", element: <NotFound /> },
   ]);
 };
 
-const MarketplaceCore: React.FC<Props> = ({ header, footer, customFallback }) => {
+const MarketplaceCore: React.FC = () => {
   return (
     <Router>
-      <CoreWrapper header={header} footer={footer}>
-        <AppRoutes customFallback={customFallback} />
+      <CoreWrapper header={Header} footer={Footer}>
+        <AppRoutes />
       </CoreWrapper>
     </Router>
   );
