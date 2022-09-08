cf78774a8975a1bf7b8365f155e0a7d78e31985d
KaWaite <kyle.waite@outlook.com>
2022-09-08 11:01:31 +0900

    fix Icon component and add Markdown

--- web/src/components/atoms/Icon/index.tsx
+++ web/src/components/atoms/Icon/index.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 import Icons from "./icons";
 
 export type Icons = keyof typeof Icons;
 
 type Props = {
+  className?: string;
   icon?: string;
   // TODO: styleの型
-  style?: any;
+  style?: CSSProperties;
+  twoToneColor?: string;
+  spin?: boolean;
 };
-const Icon: React.FC<Props> = ({ icon, style }) => {
+const Icon: React.FC<Props> = ({ className, icon, style, twoToneColor, spin }) => {
   if (!icon) return null;
-  return React.createElement(Icons[icon as Icons], { style: style });
+  const IconComponent = Icons[icon as Icons];
+  if (!IconComponent) return null;
+
+  return (
+    <IconComponent className={className} style={style} twoToneColor={twoToneColor} spin={spin} />
+  );
 };
 
 export default Icon;
