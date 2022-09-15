04f91511680faa6b8d9df19cf503788468df4c7c
Nao Yonashiro <owan.orisano@gmail.com>
2022-09-15 14:00:28 +0900

    feat(server): add m2m authentication to /api/plugins (#47)

--- server/internal/app/auth_client.go
+++ server/internal/app/auth_client.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/reearth/reearth-marketplace/server/internal/adapter"
 	"github.com/reearth/reearth-marketplace/server/internal/usecase/repo"
@@ -25,6 +27,23 @@ func authMiddleware(cfg *ServerConfig) echo.MiddlewareFunc {
 				ctx = adapter.AttachUser(ctx, u)
 			}
 			c.SetRequest(req.WithContext(ctx))
+			return next(c)
+		}
+	}
+}
+
+func serverAuthMiddleware(cfg *ServerConfig) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			ctx := req.Context()
+
+			if sub := cfg.Config.MachineAuthSub; sub != "" {
+				ai := adapter.GetAuthInfo(ctx)
+				if ai == nil || ai.Sub != sub {
+					return c.NoContent(http.StatusUnauthorized)
+				}
+			}
 			return next(c)
 		}
 	}
