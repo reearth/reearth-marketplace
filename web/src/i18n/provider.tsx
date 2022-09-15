b3800571d99be0ffb96e49fe588e43e70de30be5
KaWaite <kyle.waite@outlook.com>
2022-09-15 23:00:24 +0900

    fix i18n setup

--- web/src/i18n/provider.tsx
+++ web/src/i18n/provider.tsx
@@ -1,8 +1,23 @@
-import { ReactNode } from "react";
+import { useAuth } from "@marketplace/auth";
+import { useGetMeQuery } from "@marketplace/gql";
+import { ReactNode, useEffect } from "react";
 import { I18nextProvider } from "react-i18next";
 
 import i18n from "./i18n";
 
 export default function Provider({ children }: { children?: ReactNode }) {
+  const { isAuthenticated } = useAuth();
+  const { data } = useGetMeQuery({
+    variables: {
+      first: 50,
+    },
+    skip: !isAuthenticated,
+  });
+  const locale = data?.me.lang;
+
+  useEffect(() => {
+    i18n.changeLanguage(locale === "und" ? undefined : locale);
+  }, [locale]);
+
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
