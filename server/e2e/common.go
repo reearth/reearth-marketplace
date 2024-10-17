fa52350fee8e513991e880df6761530fa109a28f
tomokazu tantaka <t.tantaka@eukarya.io>
2024-10-17 13:24:04 +0900

    add e2e test

---
+++ server/e2e/common.go
@@ -0,0 +1,85 @@
+package e2e
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/reearth/reearth-marketplace/server/internal/app"
+	"github.com/reearth/reearth-marketplace/server/internal/infrastrcture/gcs"
+	mongorepo "github.com/reearth/reearth-marketplace/server/internal/infrastrcture/mongo"
+	"github.com/reearth/reearth-marketplace/server/internal/usecase/gateway"
+	"github.com/reearth/reearth-marketplace/server/internal/usecase/repo"
+	"github.com/reearth/reearthx/mongox"
+	"github.com/reearth/reearthx/mongox/mongotest"
+)
+
+func init() {
+	mongotest.Env = "REEARTH_DB"
+}
+
+func StartServer(t *testing.T, cfg *app.Config) *httpexpect.Expect {
+	ctx := context.Background()
+	repos, gateways := initTestReposAndGateways(t, ctx, cfg)
+	return StartServerWithRepos(t, cfg, repos, gateways)
+}
+
+func initTestReposAndGateways(t *testing.T, ctx context.Context, conf *app.Config) (*repo.Container, *gateway.Container) {
+
+	// Mongo
+	db := mongotest.Connect(t)(t)
+	client := mongox.NewClientWithDatabase(db)
+	client = client.WithTransaction()
+	repos := &repo.Container{}
+	if err := mongorepo.InitRepos(ctx, repos, db.Client(), "reearth_marketplace", mongox.IsTransactionAvailable(conf.DB)); err != nil {
+		log.Fatalf("Failed to init mongo: %+v\n", err)
+	}
+
+	// File
+	// fileRepo, err := gcs.NewFile(conf.GCS.Bucket, conf.GCS.AssetsBucket, conf.GCS.AssetsBaseURL)
+	fileRepo, err := gcs.NewFile("marketplace.test.reearth.dev", "assets.marketplace.test.reearth.dev", "")
+	if err != nil {
+		log.Fatalf("file: init error: %v", err)
+	}
+	gateways := &gateway.Container{}
+	gateways.File = fileRepo
+
+	return repos, gateways
+}
+
+func StartServerWithRepos(t *testing.T, cfg *app.Config, repos *repo.Container, gateways *gateway.Container) *httpexpect.Expect {
+	t.Helper()
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	ctx := context.Background()
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("server failed to listen: %v", err)
+	}
+	srv := app.NewServer(ctx, &app.ServerConfig{
+		Config:   cfg,
+		Repos:    repos,
+		Gateways: gateways,
+		Debug:    true,
+	})
+	ch := make(chan error)
+	go func() {
+		if err := srv.Serve(l); err != http.ErrServerClosed {
+			ch <- err
+		}
+		close(ch)
+	}()
+	t.Cleanup(func() {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			t.Fatalf("server shutdown: %v", err)
+		}
+		if err := <-ch; err != nil {
+			t.Fatalf("server serve: %v", err)
+		}
+	})
+	return httpexpect.Default(t, "http://"+l.Addr().String())
+}
